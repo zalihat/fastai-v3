525cc62874f882777e1890f1f011879ebc14ac1c
zalihat <36508180+zalihat@users.noreply.github.com>
2019-08-23 13:16:23 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1-DopqeSiSpB6lIhUBxLp-lx-svJmDNG1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-1M-b-xH9gahSoRl5gUFVbs5Pa-vwXVY'
 export_file_name = 'export.pkl'
 
 classes = ['0', '1']
